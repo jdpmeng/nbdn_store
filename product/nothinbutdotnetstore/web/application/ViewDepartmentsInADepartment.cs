4cefa367fd723cab8a92fadbefc14de08b323236
unknown <JPalmer@.Enterprise.NBTY.Global>
2010-12-09 15:15:40 -0500

    asdf

--- product/nothinbutdotnetstore/web/application/ViewDepartmentsInADepartment.cs
+++ product/nothinbutdotnetstore/web/application/ViewDepartmentsInADepartment.cs
@@ -1,13 +1,31 @@
 using System;
+using nothinbutdotnetstore.tasks;
+using nothinbutdotnetstore.tasks.stubs;
+using nothinbutdotnetstore.web.application.model;
 using nothinbutdotnetstore.web.infrastructure;
+using nothinbutdotnetstore.web.infrastructure.stubs;
 
 namespace nothinbutdotnetstore.web.application
 {
     public class ViewDepartmentsInADepartment : ApplicationCommand
     {
+        Repository repository;
+        ResponseEngine response_engine;
+
+        public ViewDepartmentsInADepartment():this(new StubRepository(),
+            new StubResponseEngine())
+        {
+        }
+
+        public ViewDepartmentsInADepartment(Repository repository, ResponseEngine response_engine)
+        {
+            this.repository = repository;
+            this.response_engine = response_engine;
+        }
+
         public void process(Request request)
         {
-            throw new NotImplementedException();
+            response_engine.prepare(repository.get_all_departments_in(request.map<Department>()));
         }
     }
 }
