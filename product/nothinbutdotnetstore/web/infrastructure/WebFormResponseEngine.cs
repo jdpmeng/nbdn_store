a84b4b66752080039d69ddf1b776398834baf7b6
Drew Petrillo <drew@gmail.com>
2010-12-10 10:48:21 -0500

    new commit

--- product/nothinbutdotnetstore/web/infrastructure/WebFormResponseEngine.cs
+++ product/nothinbutdotnetstore/web/infrastructure/WebFormResponseEngine.cs
@@ -1,17 +1,24 @@
+using System.Web;
+
 namespace nothinbutdotnetstore.web.infrastructure
 {
     public class WebFormResponseEngine : ResponseEngine
     {
         ViewFactory view_factory;
-
-        public WebFormResponseEngine(ViewFactory factory)
+        private HttpContext context;
+        public WebFormResponseEngine(ViewFactory factory, HttpContext context)
         {
             this.view_factory = factory;
+            this.context = context;
         }
 
         public void prepare<ViewModel>(ViewModel model)
         {
-            view_factory.create_view_for(model);
+
+            var handler = view_factory.create_view_for<ViewModel>(model);
+            handler.ProcessRequest(context);
         }
+
+        
     }
 }
